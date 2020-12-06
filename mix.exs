655f2d2db6c5ea50bc816f2ea1514df44f15df9c
Eric Groom <bcgroom@gmail.com>
2020-12-06 14:34:15 -0800

    add license and package config

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Inline.MixProject do
       app: :inline,
       version: "0.1.0",
       elixir: "~> 1.10",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -13,6 +15,23 @@ defmodule Inline.MixProject do
   def deps() do
     [
       {:ex_doc, "~> 0.23", only: :dev},
+    ]
+  end
+
+  def description() do
+    """
+    Inline is a tiny testing library for helping you develop faster.
+    """
+  end
+
+  @github "https://github.com/ericgroom/inline"
+
+  def package() do
+    [
+      licenses: ["MIT"],
+      links: %{github: @github},
+      source_url: @github,
+      homepage_url: @github
     ]
   end
 end
